7701bd717dc54b5f9915a0cb972b5b4de5147f84
Shelly Chawhan <shellychawhan@gmail.com>
2024-10-10 14:04:16 -0400

    Added And Connected Firebase

--- app/src/main/java/com/example/habit_tracker_prj/Register_Activity.java
+++ app/src/main/java/com/example/habit_tracker_prj/Register_Activity.java
@@ -15,10 +15,10 @@ public class Register_Activity extends AppCompatActivity {
         super.onCreate(savedInstanceState);
         EdgeToEdge.enable(this);
         setContentView(R.layout.activity_register);
-        ViewCompat.setOnApplyWindowInsetsListener(findViewById(R.id.main), (v, insets) -> {
-            Insets systemBars = insets.getInsets(WindowInsetsCompat.Type.systemBars());
-            v.setPadding(systemBars.left, systemBars.top, systemBars.right, systemBars.bottom);
-            return insets;
-        });
+//        ViewCompat.setOnApplyWindowInsetsListener(findViewById(R.id.main), (v, insets) -> {
+//            Insets systemBars = insets.getInsets(WindowInsetsCompat.Type.systemBars());
+//            v.setPadding(systemBars.left, systemBars.top, systemBars.right, systemBars.bottom);
+//            return insets;
+//        });
     }
 }
